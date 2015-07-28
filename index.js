86b82f2edc3d5cc8c93f9f5f1fdf0d751bd0d93e
yhf <xdhfying@gmail.com>
2015-07-28 16:50:08 +0800

    enable subapp as middleware

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ http = require("http");
 
 module.exports = function() {
     var index;
-    var request, response;
+    var request, response, next;
 
-    var app = function(req, res) {
+    var app = function(req, res, parentNext) {
         index = 0;
         request = req;
         response = res;
+        next = parentNext; // save parentNext
         app.next();
     };
 
@@ -26,10 +27,23 @@ module.exports = function() {
         var m = app.stack[index++];
         if (m === undefined) {
             // now at the bottom of the middleware stack
-            response.statusCode = err? 500 : 404;
-            response.end();
-            return;
+            if (next) {
+                // at the end of subapp, should return to the parent app by calling next
+                next(err);
+            } else {
+                response.statusCode = err? 500 : 404;
+                response.end();
+                return;
+            }
         } else {
+            if (m.hasOwnProperty('use')) {
+                // this middleware is a subapp
+                if (m.stack.length > 0){
+                    m(request, response, app.next);
+                } else {
+                    app.next(err);
+                }
+            }
             try {
                 if (err && m.length >= 4) {
                     m(err, request, response, app.next);
@@ -45,7 +59,6 @@ module.exports = function() {
             }
             app.next(err);
         }
-        m(request, response, app.next);
     };
 
     return app;
